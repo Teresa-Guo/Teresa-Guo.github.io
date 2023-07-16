20b49c45793e264e4b8fdfe79aa474147f459bba
RightFish <teresayyguo@gmail.com>
2023-07-16 15:38:36 +0800

    先複製廣告在下的下樓梯

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-var game = new Phaser.Game(600, 600, Phaser.AUTO, '', // 在網頁上的長 寬
+var game = new Phaser.Game(399, 399, Phaser.AUTO, 'container1',
     { preload: preload, create: create, update: update });
-
+//https://hackmd.io/@NrgG4-TCS42pma6IfQW84A/rkR_9r4Qe?type=slide#/1
 var player;
 var keyboard;
 
-var platforms = []; // 因為遊戲中平台會不斷生成和消失 所以需要用陣列來儲存
+var platforms = [];
+var grass;
 
 var leftWall;
 var leftWall2;
@@ -14,7 +15,6 @@ var rightWall2;
 
 var ceiling;
 var ceiling2;
-
 var text1;
 var text2;
 var text3;
@@ -22,26 +22,33 @@ var text3;
 var distance = 0;
 var status = 'running';
 
-window.alert("以下是小朋友下樓梯的遊戲規則：\n控制鍵盤左右鍵移動角色\n碰到釘子會扣一條命\n當生命小於0時 遊戲結束")
+var countdownSeconds = 180; //倒數計時的秒數
+var startTime = Date.now();
+var countdownInterval = null;
+var remainingSeconds = null;
+
+// window.alert("以下是小朋友下樓梯的遊戲規則：\n控制鍵盤左右鍵移動角色\n碰到釘子會扣一條命\n當生命小於0時 遊戲結束")
+
+var touchLeft = false;
+var touchRight = false;
 
 function preload () {
-
-    game.load.baseURL = 'https://Teresa-Guo.github.io/pictures/'; // 代換自己的線上.io網址
-    // game.load.baseURL = 'https://wacamoto.github.io/NS-Shaft-Tutorial/assets/';
-    //https://github.com/Teresa-Guo/Donwstairs/tree/main/new_pictures
+    //game.load.baseURL = 'https://yesfish1010.github.io/Brian591757.github.io/assets/';
+    game.load.baseURL = 'https://Brian591757.github.io/assets/';
+    //game.load.baseURL = 'https://wacamoto.github.io/NS-Shaft-Tutorial/assets/';
     game.load.crossOrigin = 'anonymous';
-
-    // spritesheet用在包含多個分別的圖片 image則是單一圖片
-    game.load.spritesheet('player', 'player.png', 32, 32); // ('圖片在線上資料夾名稱', '圖片的線上資料夾路徑', 寬像素, 高像素)
-    game.load.image('wall', 'wall.png');
-    game.load.image('ceiling', 'ceiling.png');
+    game.load.spritesheet('player', 'player.png', 32, 32);
+    game.load.image('wall_new', 'wall_new.png');
+    game.load.image('ceiling_new', 'ceiling_new.png');
+    game.load.image('grass', 'grass.png');
     game.load.image('normal', 'normal.png');
-    game.load.image('nails', 'nails.png');
-    game.load.spritesheet('conveyorRight', 'conveyor_right.png', 96, 16);
-    game.load.spritesheet('conveyorLeft', 'conveyor_left.png', 96, 16);
-    game.load.spritesheet('trampoline', 'trampoline.png', 96, 22);
-    game.load.spritesheet('fake', 'fake.png', 96, 36);
+    game.load.image('nails_new', 'nails_new.png');
+    game.load.spritesheet('conveyorRight', 'conveyor_right.png', 73, 12);
+    game.load.spritesheet('conveyorLeft', 'conveyor_left.png', 72, 12);
+    game.load.spritesheet('trampoline', 'trampoline.png', 72, 22);
+    game.load.spritesheet('fake', 'fake.png', 72, 36);
 }
+
 
 function create () {
 
@@ -60,84 +67,124 @@ function create () {
     createBounders();
     createPlayer();
     createTextsBoard();
+
+    ///加觸控
+    game.input.touch.preventDefault = false;
+    game.input.onDown.add(handleTouchStart, this)
+    game.input.onUp.add(handleTouchEnd, this)
+
+
 }
-
+//觸控
+function handleTouchStart(pointer) {
+    if (status == 'running') {
+        if (pointer.x < game.width / 2) {
+            touchLeft = true;
+            touchRight = false;
+        }
+        else if (pointer.x > game.width / 2) {
+            touchLeft = false;
+            touchRight = true;
+        }
+        else {
+            touchLeft = false;
+            touchRight = false;
+        }
+    }
+    else if (status == 'gameOver') {
+        restart();
+    }
+}
+//觸控放開
+function handleTouchEnd(pointer) {
+        touchLeft = false;
+        touchRight = false;    
+}
 function update () {
-
+    console.log(status);
+ //   console.log("A");
+    updateTextsBoard();
+ //   console.log(status)
+    if(status == 'finish') return;
     // bad
-    if(status == 'gameOver' && keyboard.enter.isDown) restart();
+    if (status == 'gameOver' && keyboard.enter.isDown) restart();
+
+ //   game.input.onDown.add(handleTouchStart, this)
     if(status != 'running') return;
 
-    this.physics.arcade.collide(player, platforms, effect); // 檢測玩家和平台的碰撞 並執行effect函式
-    this.physics.arcade.collide(player, [leftWall,leftWall2, rightWall,rightWall2]);
+
+    this.physics.arcade.collide(player, platforms, effect);
+    this.physics.arcade.collide(player, [leftWall, rightWall, rightWall2, leftWall2]);
     checkTouchCeiling(player);
     checkGameOver();
 
     updatePlayer();
     updatePlatforms();
-    updateTextsBoard();
+    // updateTextsBoard();
 
     createPlatforms();
 }
 
 function createBounders () {
-    leftWall = game.add.sprite(0, 0, 'wall'); // sprite為創造的遊戲精靈 (網頁上座標, '圖片名稱')
-    leftWall2 = game.add.sprite(0, 200, 'wall');
-    game.physics.arcade.enable(leftWall); // 啟用物理引擎對leftWall和其他物體進行碰撞處理
+    leftWall = game.add.sprite(0, 0, 'wall_new');
+    leftWall2 = game.add.sprite(0, 400, 'wall_new');
+    game.physics.arcade.enable(leftWall);
     game.physics.arcade.enable(leftWall2);
-    leftWall.body.immovable = true; // 牆是靜止的
+    leftWall.body.immovable = true;
     leftWall2.body.immovable = true;
 
-    rightWall = game.add.sprite(583, 0, 'wall');
-    rightWall2 = game.add.sprite(583, 200, 'wall');
+    rightWall = game.add.sprite(382, 0, 'wall_new');
     game.physics.arcade.enable(rightWall);
     rightWall.body.immovable = true;
+    rightWall2 = game.add.sprite(382, 400, 'wall_new');
     game.physics.arcade.enable(rightWall2);
     rightWall2.body.immovable = true;
 
-    ceiling = game.add.image(0, 0, 'ceiling');
-    ceiling2 = game.add.image(200, 0, 'ceiling');
+    ceiling = game.add.image(18, 0, 'ceiling_new');
+    //ceiling = game.add.image(399, 0, 'ceiling_new');
 }
 
-var lastTime = 0; // 用來記錄上一次創建平台的時間
-function createPlatforms () {
-    if(lastTime == 0) { // 一開始就生成兩個平台在450 550的座標
+var lastTime = 0;
+var startfloor = 0
+function createPlatforms() {
+ //   console.log(game.time)
+    if (lastTime == 0) {
         lastTime = game.time.now;
-        createOnePlatform(450);
-        createOnePlatform(550);
+        createOnePlatform(320);
+        createOnePlatform(400);
+      
+    }
+    if(game.time.now > lastTime + 500) {
+        lastTime = game.time.now;
+        createOnePlatform(400);
         distance += 1;
     }
-
-    if(game.time.now > lastTime + 600) { // 生成平台時間越少 平台之間垂直距離越短 每隔600毫秒創造一個新平台
-        lastTime = game.time.now;
-        createOnePlatform(600);
-        distance += 1; // distance用來記錄現在往下到第幾個平台
-    }
 }
 
-function createOnePlatform (createTime) {
+
+function createOnePlatform (h) {
 
     var platform;
-    var x = Math.random()*(600 - 96 - 40) + 20; // 平台生成x座標
-    var y = createTime; // 平台生成y座標
-    var rand = Math.random() * 100; // Math.random()會隨機產生0 1數字 所以代表隨機生成各種平台之機率
+    var x = Math.random()*(399 - 96 - 40) + 20;
+    var y = h;
+    var rand = Math.random() * 100;
 
-    if(rand < 30) { // 可改機率
-        platform = game.add.sprite(x, y, 'normal');
+    if(rand < 30) {
+        platform = game.add.sprite(x, y, 'grass');
     } else if (rand < 40) {
-        platform = game.add.sprite(x, y, 'nails');
+        platform = game.add.sprite(x, y, 'nails_new');
         game.physics.arcade.enable(platform);
-        platform.body.setSize(96, 15, 0, 15); // (物理碰撞的寬度, 碰撞高度, 相對platform的水平偏移量, 垂直偏移量)
+        platform.body.setSize(72, 15, 0, 0);
+        //platform.body.setSize(72, 15, 0, 15);
     } else if (rand < 50) {
         platform = game.add.sprite(x, y, 'conveyorLeft');
-        // 下行可以達成輸送帶在傳輸的效果
-        platform.animations.add('scroll', [0, 1, 2, 3], 16, true); // ('動畫名稱', [conveyerLeft spritesheet上各圖片分別的編號], 動畫播放速度, 無限循環播放)
+        platform.animations.add('scroll', [0, 1, 2, 3], 16, true);
         platform.play('scroll');
     } else if (rand < 60) {
         platform = game.add.sprite(x, y, 'conveyorRight');
         platform.animations.add('scroll', [0, 1, 2, 3], 16, true);
         platform.play('scroll');
-    } else if (rand < 80) {
+    } else if (rand < 90) {
         platform = game.add.sprite(x, y, 'trampoline');
         platform.animations.add('jump', [4, 5, 4, 3, 2, 1, 0, 1, 2, 3], 120);
         platform.frame = 3;
@@ -147,19 +194,19 @@ function createOnePlatform (createTime) {
     }
 
     game.physics.arcade.enable(platform);
-    platform.body.immovable = true; // 一般平台在碰撞時不會移動
+    platform.body.immovable = true;
     platforms.push(platform);
 
-    platform.body.checkCollision.down = false; // 禁用一般平台與下方物體的碰撞檢測
+    platform.body.checkCollision.down = false;
     platform.body.checkCollision.left = false;
     platform.body.checkCollision.right = false;
 }
 
 function createPlayer() {
-    player = game.add.sprite(300, 50, 'player'); // 玩家初始位置
+    player = game.add.sprite(200, 50, 'player');
     game.physics.arcade.enable(player);
-    player.body.gravity.y = 500; // 重力變小 掉下來速度變小
-    player.animations.add('left', [0, 1, 2, 3], 8); // ('動畫名稱', [player spritesheet上各圖片分別的編號], 動畫撥放速度)
+    player.body.gravity.y = 450;
+    player.animations.add('left', [0, 1, 2, 3], 8);
     player.animations.add('right', [9, 10, 11, 12], 8);
     player.animations.add('flyleft', [18, 19, 20, 21], 12);
     player.animations.add('flyright', [27, 28, 29, 30], 12);
@@ -171,17 +218,21 @@ function createPlayer() {
 
 function createTextsBoard () {
     var style = {fill: '#ff0000', fontSize: '20px'}
-    text1 = game.add.text(20, 10, '', style); // 地下幾個平台字樣
-    text2 = game.add.text(550, 10, '', style); // 生命數字樣
-    text3 = game.add.text(225, 280, 'Enter 重新開始', style); // 重新開始字樣
+    text1 = game.add.text(10, 10, '', style);
+    text2 = game.add.text(345, 10, '', style);
+    text3 = game.add.text(78, 180, 'Enter 鍵或點擊螢幕重新開始', style);
     text3.visible = false;
 }
 
 function updatePlayer () {
-    if(keyboard.left.isDown) { // isDown代表是否有按下左鍵
-        player.body.velocity.x = -400; // 玩家控制鍵盤的往左移動速度
-    } else if(keyboard.right.isDown) {
-        player.body.velocity.x = 400; // 玩家控制鍵盤的往右移動速度
+    if(keyboard.left.isDown)  {
+        player.body.velocity.x = -500;
+    } else if (touchLeft == true) {
+        player.body.velocity.x = -500;
+    } else if (touchRight == true) {
+        player.body.velocity.x = 500;
+    } else if (keyboard.right.isDown) {
+        player.body.velocity.x = 500;
     } else {
         player.body.velocity.x = 0;
     }
@@ -215,17 +266,35 @@ function setPlayerAnimate(player) {
 function updatePlatforms () {
     for(var i=0; i<platforms.length; i++) {
         var platform = platforms[i];
-        platform.body.position.y -= 3; // 平台往上的速度
-        if(platform.body.position.y <= -20) { // 平台往上跑到-20之上時 平台上移到天花板了
-            platform.destroy(); // 刪掉往上跑的平台
-            platforms.splice(i, 1); // 從陣列移出第i個元素
+        platform.body.position.y -= 3;
+        if(platform.body.position.y <= 18) {
+            platform.destroy();
+            platforms.splice(i, 1);
         }
     }
 }
 
 function updateTextsBoard () {
     text1.setText('life:' + player.life);
-    text2.setText('B' + distance);
+
+    var currentTime = Date.now();
+    var elapsedTime = currentTime - startTime;
+
+    var remainingSeconds = Math.max(0, countdownSeconds - Math.floor(elapsedTime / 1000));
+    if(remainingSeconds == 0){
+        status = 'finish';
+        showMyDialog(); // 彈出 <dialog>
+        setTimeout(function() {
+            window.location.href = 'https://www.surveycake.com/s/KO9Lv';
+        }, 2000);
+    }
+
+    var minutes = Math.floor(remainingSeconds / 60);
+    var seconds = remainingSeconds % 60;
+
+    // 轉為 mm:ss 的形式
+    var formattedTime = ("0" + minutes).slice(-2) + ":" + ("0" + seconds).slice(-2);
+    text2.setText(formattedTime);
 }
 
 function effect(player, platform) {
@@ -238,7 +307,7 @@ function effect(player, platform) {
     if(platform.key == 'trampoline') {
         trampolineEffect(player, platform);
     }
-    if(platform.key == 'nails') {
+    if(platform.key == 'nails_new') {
         nailsEffect(player, platform);
     }
     if(platform.key == 'normal') {
@@ -250,23 +319,23 @@ function effect(player, platform) {
 }
 
 function conveyorRightEffect(player, platform) {
-    player.body.x += 2; // 輸送帶使玩家右移速度
+    player.body.x += 2;
 }
 
 function conveyorLeftEffect(player, platform) {
-    player.body.x -= 2; // 輸送帶使玩家左移速度
+    player.body.x -= 2;
 }
 
 function trampolineEffect(player, platform) {
     platform.animations.play('jump');
-    player.body.velocity.y = -300; // 彈跳床使玩家上移速度
+    player.body.velocity.y = -300;
 }
 
 function nailsEffect(player, platform) {
     if (player.touchOn !== platform) {
         player.life -= 3;
         player.touchOn = platform;
-        game.camera.flash(0xff0000, 100); //背景閃爍(顏色色碼, 毫秒)
+        game.camera.flash(0xff0000, 100);
     }
 }
 
@@ -283,27 +352,27 @@ function fakeEffect(player, platform) {
     if(player.touchOn !== platform) {
         platform.animations.play('turn');
         setTimeout(function() {
-            platform.body.checkCollision.up = false; // 禁用玩家在假平台上方的碰撞檢測 看起來就像直接穿過假平台
+            platform.body.checkCollision.up = false;
         }, 100);
         player.touchOn = platform;
     }
 }
 
 function checkTouchCeiling(player) {
-    if(player.body.y < 0) { // 若玩家處碰到天花板
-        if(player.body.velocity.y < 0) { // 若玩家目前速度是往上
-            player.body.velocity.y = 0; // 把玩加速度設成0
+    if(player.body.y < 0) {
+        if(player.body.velocity.y < 0) {
+            player.body.velocity.y = 0;
         }
         if(game.time.now > player.unbeatableTime) {
             player.life -= 3;
             game.camera.flash(0xff0000, 100);
-            player.unbeatableTime = game.time.now + 2000; // unbeatableTime為玩家無敵時間
+            player.unbeatableTime = game.time.now + 2000;
         }
     }
 }
 
 function checkGameOver () {
-    if(player.life <= 0 || player.body.y > 500) { // 到底端會死掉
+    if(player.life <= 0 || player.body.y > 399) {
         gameOver();
     }
 }
@@ -320,4 +389,17 @@ function restart () {
     distance = 0;
     createPlayer();
     status = 'running';
+}
+
+// 關於時間結束後的彈出視窗之設定
+function showMyDialog() {
+    var dialog = document.getElementById('myDialog');
+    dialog.style.display = 'block'; // 顯示 <dialog>
+    dialog.showModal(); // 啟用模態對話框
+}
+
+// 在特定條件滿足時隱藏 <dialog>
+function hideMyDialog() {
+    var dialog = document.getElementById('myDialog');
+    dialog.close(); // 隱藏 <dialog>
 }
